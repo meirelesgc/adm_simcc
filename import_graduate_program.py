964971e048fb4508c60f04b7708fadfa48f59414
meirelescg <geu_costa@outlook.com>
2024-03-20 02:24:14 -0300

    UFSB Graduate_program

--- import_graduate_program.py
+++ import_graduate_program.py
@@ -32,34 +32,62 @@ def select_type_graduate_program(serie_gp: pd.Series) -> str:
     return str("/").join(type), max_rating
 
 
+def sanitize_string(string: str) -> str:
+    sanitized_string = string.strip().replace("'", "")
+    return sanitized_string
+
+
 if __name__ == "__main__":
-    data_frame_institutions = get_institutions()
-    for Index, Data in data_frame_institutions.iterrows():
-        print(f"[{Index}] - {Data['name']} - {Data['acronym']}")
-    institution_index = int(
-        input(f"Anexar os programas de graduação na instituição [Index - 0/{Index}]: ")
-    )
-    data_frame_institutions = data_frame_institutions.iloc[institution_index]
+    if int(input("Cadastrar programas de graduação?\n[1-Sim / 0-Não]: ")):
+        data_frame_institutions = get_institutions()
+        for Index, Data in data_frame_institutions.iterrows():
+            print(f"[{Index}] - {Data['name']} - {Data['acronym']}")
+        institution_index = int(
+            input(
+                f"Anexar os programas de graduação na instituição [Index - 0/{Index}]: "
+            )
+        )
+        data_frame_institutions = data_frame_institutions.iloc[institution_index]
 
-    localização = str(input("Digite a cidade do programa: "))
+        localização = str(input("Digite a cidade do programa: "))
 
-    for Index, Data in get_graduate_program().iterrows():
+        for Index, Data in get_graduate_program().iterrows():
 
-        type = select_type_graduate_program(Data)
+            type = select_type_graduate_program(Data)
 
-        script_sql = f"""
-            INSERT INTO graduate_program(
-            code, name, area, modality, type, rating, institution_id, city, instituicao, sigla)
-            VALUES 
-                ('{Data['Código']}', 
-                '{Data['Programa']}', 
-                '{Data['Área de Avaliação'].strip()}', 
-                '{Data['Modalidade']}', 
-                '{type[0]}', 
-                '{type[1]}', 
-                '{data_frame_institutions['institution_id']}', 
-                '{localização}', 
-                '{Data["Instituição de Ensino"]}', 
-                '{data_frame_institutions["acronym"]}')
-            """
-        bd.db_script(script_sql=script_sql)
+            script_sql = f"""
+                INSERT INTO graduate_program(
+                code, name, area, modality, type, rating, institution_id, city, instituicao, sigla)
+                VALUES 
+                    ('{Data['Código']}', 
+                    '{Data['Programa']}', 
+                    '{Data['Área de Avaliação'].strip()}', 
+                    '{Data['Modalidade']}', 
+                    '{type[0]}', 
+                    '{type[1]}', 
+                    '{data_frame_institutions['institution_id']}', 
+                    '{localização}', 
+                    '{Data["Instituição de Ensino"]}', 
+                    '{data_frame_institutions["acronym"]}')
+                """
+            bd.db_script(script_sql=script_sql)
+
+    if int(
+        input("Cadastrar pesquisadores nos programas de graduação?\n[1-Sim / 0-Não]: ")
+    ):
+        data_frame_pesquisadores = pd.read_excel(
+            "Files/graduate_program_researcher_ufsb.xlsx"
+        ).dropna(how="all")
+        data_frame_pesquisadores["Lattes"] = data_frame_pesquisadores["Lattes"].astype(
+            str
+        )
+        for Index, Data in data_frame_pesquisadores.iterrows():
+            if Data["Lattes"] != "nan":
+                script_sql = f"""
+                INSERT INTO graduate_program_researcher (graduate_program_id, researcher_id, year, type_)
+                SELECT gp.graduate_program_id, r.researcher_id, {2024}, '{Data['type_']}'
+                FROM researcher r
+                JOIN graduate_program gp
+                ON gp.code = '{sanitize_string(Data['Code'])}'
+                WHERE r.lattes_id = '{sanitize_string(Data['Lattes'])}';"""
+                bd.db_script(script_sql=script_sql)
