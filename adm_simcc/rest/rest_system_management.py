b4ed5683081274595ac61491e04bcbbcb41f2625
meirelescg <geu_costa@outlook.com>
2024-05-29 18:17:28 -0300

    Ajustes na chamada de sistema

--- adm_simcc/rest/rest_system_management.py
+++ adm_simcc/rest/rest_system_management.py
@@ -2,19 +2,30 @@ import os
 import subprocess
 
 from http import HTTPStatus
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_cors import cross_origin
 
 rest_sys = Blueprint("rest_system_management", __name__, url_prefix="/sys")
 
 
-@rest_sys.route("/requestUpdate", methods=["GET"])
+@rest_sys.route("/checkHop", methods=["GET"])
 @cross_origin(origin="*", headers=["Content-Type"])
 def requestUpdate():
     line = read_log(os.environ["HOP_LOG_PATH"])
     if line and line != "ok":
         return jsonify({"message": line}), HTTPStatus.OK
     else:
+        return jsonify({"message": "hop em modo de espera"}), HTTPStatus.OK
+
+
+@rest_sys.route("/requestUpdate", methods=["GET"])
+@cross_origin(origin="*", headers=["Content-Type"])
+def requestUpdate():
+    onHop = request.get_json()
+    line = read_log(os.environ["HOP_LOG_PATH"])
+    if line and line != "ok":
+        return jsonify({"message": "hop em uso"}), HTTPStatus.OK
+    elif onHop["state"]:
         subprocess.Popen(["/usr/local/sbin/Jade-Extrator-Routine.sh",],shell=True,)  # fmt: skip
         return jsonify({"message": "processo de carga iniciado"}), HTTPStatus.OK
 
