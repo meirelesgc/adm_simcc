e5c6acbd8c29429fcae38f5442937dd30753bdcb
meirelescg <geu_costa@outlook.com>
2024-06-05 12:14:03 -0300

    arquivo de configuração do administrativo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from adm_simcc import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
