cb40f7ecd9daba2728027b7a3685672160567717
meirelesgc <geu_costa@outlook.com>
2024-09-03 17:12:40 -0300

    a lista era em outro lugar

--- adm_simcc/dao/dao_graduate_program_student.py
+++ adm_simcc/dao/dao_graduate_program_student.py
@@ -105,7 +105,7 @@ def student_update(
     parameters = list()
 
     # fmt: 0ff
-    for researcher in ListGraduateProgramStudent.researcher_list:
+    for researcher in ListGraduateProgramStudent.student_list:
         parameters.append(
             (
                 researcher.year,
